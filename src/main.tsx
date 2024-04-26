51e8ba5b1e08aef792a8224ad7419f7a96fb3ff8
Punler Pich <punlerpich27@gmail.com>
2024-04-26 10:00:35 -0700

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { NextUIProvider } from "@nextui-org/react";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
